1bbc76228871425874504d7ba1030de30b29318b
James <e8315402@gmail.com>
2017-11-19 18:32:26 +0800

    修改 test framework UT -> Mocha + nyc AT -> Zombie + cucumber

---
+++ vue-paper-dashboard-master/test/e2e/step_definitions/example_steps.js
@@ -0,0 +1,12 @@
+var {defineSupportCode} = require('cucumber')
+
+defineSupportCode(function ({Given, When, Then}) {
+  Given('I am on the Cucumber.js GitHub repository', function () {
+  })
+
+  When('I click on {stringInDoubleQuotes}', function (text) {
+  })
+
+  Then('I should see {stringInDoubleQuotes}', function (text) {
+  })
+})
