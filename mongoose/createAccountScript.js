6bec0be4a6226fc94086d16fe0528d992d2ab427
disc9562 <disc9562@gmail.com>
2017-12-10 13:29:47 +0800

    新增 塞Account script

---
+++ mongoose/createAccountScript.js
@@ -0,0 +1,112 @@
+let accountList = [
+  {
+    username:"蔣偉博",
+    password:"123456",
+    email:"letitgo@gmail.com",
+    id:"107598001",
+    role: ["student"],
+    TA: "No"
+  },
+  {
+    username:"蔣友伲",
+    password:"123456",
+    email:"letitgo@gmail.com",
+    id:"107598002",
+    role: ["student"],
+    TA: "No"
+  },
+  {
+    username:"陳政好",
+    password:"123456",
+    email:"letitgo@gmail.com",
+    id:"107598003",
+    role: ["student"],
+    TA: "No"
+  },
+  {
+    username:"連預先",
+    password:"123456",
+    email:"letitgo@gmail.com",
+    id:"107598004",
+    role: ["student"],
+    TA: "No"
+  },
+  {
+    username:"王裕响",
+    password:"123456",
+    email:"letitgo@gmail.com",
+    id:"107598005",
+    role: ["student"],
+    TA: "No"
+  },
+  {
+    username:"王美",
+    password:"123456",
+    email:"letitgo@gmail.com",
+    id:"107598101",
+    role: ["student"],
+    TA: "No"
+  },
+  {
+    username:"李麗",
+    password:"123456",
+    email:"letitgo@gmail.com",
+    id:"107598102",
+    role: ["student"],
+    TA: "No"
+  },
+  {
+    username:"黃怡",
+    password:"123456",
+    email:"letitgo@gmail.com",
+    id:"107598103",
+    role: ["student"],
+    TA: "No"
+  },
+  {
+    username:"Jay",
+    password:"123456",
+    email:"eatChicken@gmail.com",
+    id:"T05598053",
+    role: ["teacher"],
+    TA: "No"
+  },
+  {
+    username:"Wayne",
+    password:"123456",
+    email:"oneForAll@gmail.com",
+    id:"T05598054",
+    role: ["teacher"],
+    TA: "No"
+  },
+  {
+    username:"Hank",
+    password:"123456",
+    email:"IamAwesome@gmail.com",
+    id:"T05598055",
+    role: ["teacher"],
+    TA: "Yes"
+  },
+  {
+    username:"JohnSon",
+    password:"123456",
+    email:"IamSuperMan@gmail.com",
+    id:"105598056",
+    role: ["student"],
+    TA: "Yes"
+  },
+]
+
+const axios = require('axios')
+
+function initAxiosPromise () {
+  return accountList.map(function (eachAccount) {
+    return axios.post('http://localhost:9090/api/signupSE', eachAccount)
+  })
+}
+axios.all(initAxiosPromise())
+.then(function (response) {
+  console.log('create fake account success')
+}).catch(function (err) {
+  console.log(err)
+})
