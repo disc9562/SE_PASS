760ade10603aa3dcf0cef09fe12eeb7724927610
系統實驗室軟體 <>
2017-12-30 16:20:10 +0800

    add apiTest

--- mongoose/test/seCourseInfo.test.js
+++ mongoose/test/seCourseInfo.test.js
@@ -195,5 +195,69 @@ describe('There has students in the course',function(){
 })
 
 describe('There has students in the course',function(){
-    
+    it('add duplicate student into course',function(done){
+        let seCourseInfo = mongoose.model('SeCourseInfo')
+        let seCourseInfoMock = sinon.mock(seCourseInfo)
+        let seAccount = mongoose.model('SeAccount')
+        let SeAccountMock = sinon.mock(seAccount)
+
+        SeAccountMock.expects('find')
+        .withArgs({'id':'105598017'})
+        .resolves([{
+            "_id": "5a19259f9b399e03a04521aa",
+            "__v": 0,
+            "username": "hk01",
+            "password": "1234",
+            "email": "hk01@gmail.com",
+            "id": "105598017",
+            "timeCreated": "2017-11-25T08:11:11.060Z",
+            "TA": "No",
+            "role": [
+                "student"
+            ],
+            'course':['5a1eaad63fa9c62328b3562d']
+            }
+        ])
+
+        seCourseInfoMock.expects('find')
+            .withArgs({'courseId':'5a1eaad63fa9c62328b3562d'})
+            .resolves([
+                {
+                    "__v": 0,
+                    "courseId": "5a1eaad63fa9c62328b3562d",
+                    "_id": "5a1ff5a1a0afaf45d499f15f",
+                    "students": [
+                        {
+                            "_id": "5a1eabc5786e8b2ab496e5ed",
+                            "__v": 0,
+                            "username": "disc9562",
+                            "password": "12345678",
+                            "email": "disc121239562@gmail.com",
+                            "id": "105598017",
+                            "timeCreated": "2017-11-29T12:44:53.811Z",
+                            "TA": "No",
+                            "role": [
+                                "admin"
+                            ],
+                            "course":['5a1eaad63fa9c62328b3562d']
+                        }
+                    ]
+                }
+            ])
+            chai.request(app)
+            .post('/api/addStudentIntoCourse')
+            .send({
+                'studentId':'105598017',
+                'courseId':'5a1eaad63fa9c62328b3562d'
+            }).end(function(err, res){
+                SeAccountMock.verify()
+                SeAccountMock.restore()            
+                seCourseInfoMock.verify()
+                seCourseInfoMock.restore()
+                expect(res.status).to.equal(200);
+                expect(res.body).deep.equal({"error": "student is already in the course"});
+                
+                done()
+            })
+    })
 })
